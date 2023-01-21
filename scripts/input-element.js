ad491eac422071c80f41e95ee1e29c6c8da88a49
Pepe <danil-danil-korolev@bk.ru>
2023-01-21 20:44:13 +0700

    register & login pages completed

--- scripts/input-element.js
+++ scripts/input-element.js
@@ -7,11 +7,28 @@ document.addEventListener('DOMContentLoaded', () => {
     parentElement.classList.toggle('_not-empty', !!value)
   }
 
+  function togglePasswordVisibility(targetInput, parentElement, event) {
+    if (targetInput.type === 'password') {
+      targetInput.type = 'text'
+      return parentElement.classList.add('_show-password')
+    }
+
+    targetInput.type = 'password'
+    parentElement.classList.remove('_show-password')
+  }
+
   if (inputElements.length) {
     inputElements.forEach(inputElement => {
       const inputElementInput = inputElement.querySelector('[data-input-element-input]')
+      const inputElementHidePasswordButtons = inputElement.querySelectorAll('[data-input-element-hide-password]')
 
       inputElementInput.addEventListener('input', changeInputElementHandler.bind(null, inputElement))
+
+      if (inputElementHidePasswordButtons.length) {
+        inputElementHidePasswordButtons.forEach(inputElementHidePasswordButton => {
+          inputElementHidePasswordButton.addEventListener('click', togglePasswordVisibility.bind(null, inputElementInput, inputElement))
+        })
+      }
     })
   }
 })
